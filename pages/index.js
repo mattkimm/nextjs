f43af72891c7b0ab101d311ec7b3910f7cf61af3
mattkimm <matt85kim53@gmail.com>
2021-05-04 10:48:24 +0900

    getServerSideProps added into detail page

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,19 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 import ItemList from "../src/component/ItemList";
-import { Divider, Header } from "semantic-ui-react";
+import { Divider, Header, Loader } from "semantic-ui-react";
 
 // _app 에서  meta 태그나 title 속성같은걸 정의할 떄는 next/head를 불러와서 사용해야한다.
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
   function getData() {
     axios.get(API_URL).then((res) => {
-      console.log(res.data);
       setList(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -26,19 +27,31 @@ export default function Home() {
   }, []);
 
   return (
-    <>
+    <div>
       <Head>
         <title>HOME | 코딩 </title>
+        <meta name="description" content="test" />
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <ItemList list={list.slice(9)} />
-    </>
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
+    </div>
   );
 }
--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,40 +1,85 @@
 import axios from "axios";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
 import Item from "../../src/component/Item";
 
 /* 
-dynamic Router : post/1 으로 routing 하려면 next/router useRotuer 함수 사용
-
+@@dynamic Router : post/1 으로 routing 하려면 next/router useRotuer 함수 사용
 reference : https://nextjs.org/docs/routing/dynamic-routes
-
-
 Client-side navigations to dynamic routes are handles with 'next/link'
+
+@@ getServersideProps (Server-side rednering)
+getServerSideProps (Server-side Rendering): Fetch data on each request.
+
+If you export an async function called getServerSideProps from a page,
+Next.js will pre-render this page on each request using the
+data returned by getServerSideProps.
+
+'context' parameter is an object [contains key]
+
 */
 
-const Post = () => {
-  const router = useRouter();
-  const { id } = router.query;
+const Post = ({ item }) => {
+  //const router = useRouter();
+  //const { id } = router.query;
 
-  const [item, setItem] = useState({});
+  //const [item, setItem] = useState({});
+  //const [isLoading, setIsLoading] = useState(true);
 
-  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  // const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      setItem(res.data);
-    });
-  }
+  // function getData() {
+  //   axios.get(API_URL).then((res) => {
+  //     setItem(res.data);
+  //     setIsLoading(false);
+  //   });
+  // }
 
-  useEffect(() => {
-    if (id && id > 0) {
-      getData();
-    }
+  // useEffect(() => {
+  //   if (id && id > 0) {
+  //     getData();
+  //   }
 
-    return () => {};
-  }, [id]);
+  //   return () => {};
+  // }, [id]);
 
-  return <Item item={item} />;
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
+  //  {isLoading ? (
+  //   <div style={{ padding: "300px 0" }}>
+  //     <Loader inline="centered" active>
+  //       Loading
+  //     </Loader>
+  //   </div>
+  // ) : (
+  //   <Item item={item} />
+  // )}
 };
 
 export default Post;
+
+/*@@ getServersideProps (Server-side rednering) */
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+  return {
+    props: {
+      item: data,
+    }, // will be passed to the page component as props.
+  };
+}
