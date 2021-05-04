c1adaa54bb45e4a818ca549df6d852d4889b9e84
mattkimm <matt85kim53@gmail.com>
2021-05-04 11:08:11 +0900

    fixed err

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -19,6 +19,9 @@ data returned by getServerSideProps.
 
 'context' parameter is an object [contains key]
 
+
+s
+
 */
 
 const Post = ({ item }) => {
@@ -71,7 +74,7 @@ const Post = ({ item }) => {
 
 export default Post;
 
-/*@@ getServersideProps (Server-side rednering) */
+/*@@ getServersideProps (Server-side rendering) */
 export async function getServerSideProps(context) {
   const id = context.params.id;
   const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
@@ -83,3 +86,21 @@ export async function getServerSideProps(context) {
     }, // will be passed to the page component as props.
   };
 }
+
+/*
+Next js 모든 페이지 사전 렌더링 (Pre-rendering)
+더 좋은 퍼포먼스
+검색엔진최적화(SEO)
+1. 정적 생성
+2. Server Side Rendering (SSR, Dynamic Rendering)
+차이점은 언제 html 파일을 생성하는가
+[정적 생성]
+- 프로젝트가 빌드하는 시점에 html파일들이 생성
+- 모든 요청에 재사용
+- 퍼포먼스 이유로, 넥스트 js는 정적 생성을 권고
+- 정적 생성된 페이지들은 CDN에 캐시
+- getStaticProps / getStaticPaths
+[서버사이드 렌더링]은 매 요청마다 html 을 생성
+- 항상 최신 상태 유지
+- getServerSideProps
+ */
