237b53632521a3b654c4064d9815a6b4515cf6ad
mattkimm <matt85kim53@gmail.com>
2021-05-04 11:07:43 +0900

    useRouter, activeItem added

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,32 +1,38 @@
+import { useRouter } from "next/router";
 import React from "react";
 import { Menu } from "semantic-ui-react";
 
+/*
+a tag 나 location을 이동을 시키면 페이지가 새로고침되면서 이동한다.
+nextlink를 이용하면 page 가 그대로 있는상태에서 안에 내용물만 변경된다.
+
+*/
 function Gnb() {
-  const activeItem = "home";
+  const router = useRouter();
+  let activeItem;
+
+  if (router.pathname === "/") {
+    activeItem = "home";
+  } else if (router.pathname === "/about") {
+    activeItem = "about";
+  }
+
+  function goLink(e, data) {
+    if (data.name === "home") {
+      router.push("/");
+    } else if (data.name === "about") {
+      router.push("/about");
+    }
+  }
+
   return (
     <Menu>
-      <Menu.Item
-        name="editorials"
-        active={activeItem === "editorials"}
-        //onClick={this.handleItemClick}
-      >
-        Editorials
+      <Menu.Item name="home" active={activeItem === "home"} onClick={goLink}>
+        Home
       </Menu.Item>
 
-      <Menu.Item
-        name="reviews"
-        active={activeItem === "reviews"}
-        //onClick={this.handleItemClick}
-      >
-        Reviews
-      </Menu.Item>
-
-      <Menu.Item
-        name="upcomingEvents"
-        active={activeItem === "upcomingEvents"}
-        //onClick={this.handleItemClick}
-      >
-        Upcoming Events
+      <Menu.Item name="about" active={activeItem === "about"} onClick={goLink}>
+        About
       </Menu.Item>
     </Menu>
   );
